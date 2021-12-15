d90e1c3d11a4904a246196779389bcdf503b57b5
venkatakireeti <Venkata@getluckyvr.com>
2021-12-15 12:19:34 -0500

    latest

--- src/index.js
+++ src/index.js
@@ -8,25 +8,34 @@ import { ConnectedRouter } from "connected-react-router";
 import * as queryString from 'query-string';
 
 async function main() {
-  const urlParams = queryString.parse(window.location.search);
-  const response = await fetch("http://ec2-18-217-55-36.us-east-2.compute.amazonaws.com:8081/api/metadatas/login/url");
-  const url = await response.text();
-  let user = undefined;
-  if (urlParams.error) {
-    console.log(`An error occurred: ${urlParams.error}`);
-  } else {
-    console.log(`The code is: ${urlParams.code}`);
+  const accessToken = localStorage.getItem("token");
+  let componentToLoad = <App history={getHistory()} />
+  console.log(accessToken);
+  if(!accessToken) {
+    const urlParams = queryString.parse(window.location.search);
     if(urlParams.code) {
-    const response = await fetch("http://ec2-18-217-55-36.us-east-2.compute.amazonaws.com:8081/api/metadatas/login/code?code="+urlParams.code);
+      const response = await fetch("http://ec2-18-217-55-36.us-east-2.compute.amazonaws.com:8081/api/metadatas/login/code?code="+urlParams.code);
+      const data = await response.json();
+      console.log(data);
+      localStorage.setItem("token", data.accessToken);
+    } else {
+      const response = await fetch("http://ec2-18-217-55-36.us-east-2.compute.amazonaws.com:8081/api/metadatas/login/url");
+      const url = await response.text();
+      console.log(url);
+      componentToLoad = <App url={url} history={getHistory()} />;
+    }
+  } else {
+    const response = await fetch("http://ec2-18-217-55-36.us-east-2.compute.amazonaws.com:8081/api/metadatas/user",{
+      method: 'GET', // *GET, POST, PUT, DELETE, etc.
+      headers: {
+        'x-access-token': accessToken
+      }
+    })
     const data = await response.json();
     console.log(data);
-    user = data.user.name;
-    localStorage.setItem("token", data.accessToken);
     localStorage.setItem("user", data.user);
-    }
+    componentToLoad = <App user={data.user} history={getHistory()} />;
   }
-  console.log(url);
-  const componentToLoad = <App url={url} user ={user} history={getHistory()} />;
   const store = createStore();
   ReactDOM.render(
     <Provider store={store}>
